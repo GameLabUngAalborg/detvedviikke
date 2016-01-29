9f1689754bbeea5d4b78601970b2a55f01862707
sdfdsf <lol@lol.lol>
2016-01-29 15:21:43 +0100

    test

---
+++ Assets/fOOD.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Food  : MonoBehaviour {
+
+
+	public float FoodAmount = 0.5f;
+
+
+
+
+	// Use this for initialization
+	void OnTriggerEnter (Collider ObjectThatTouchedMe)
+	{
+		if (ObjectThatTouchedMe.tag == "Player") 
+		{
+			ObjectThatTouchedMe.GetComponent<Teacher.Hunger>().HungerBar.value += FoodAmount;
+			Destroy(gameObject);
+		}
+	}
+
+}
+
+
+
+
+
