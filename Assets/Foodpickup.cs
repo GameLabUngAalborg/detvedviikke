a876346c7e3ad65d19e130d3459ed6d511d901d7
fvgcfg <hyftyhf@fghf.vgfh>
2016-02-11 17:27:15 +0100

    no message

---
+++ Assets/Foodpickup.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Foodpickup : MonoBehaviour {
+
+	private Teacher.ResourceController ResourceController;
+	public int AmountOfood = 10;
+
+	void Awake()
+	{
+		ResourceController = GameObject.Find("ResourceController").GetComponent<Teacher.ResourceController>();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player") // if the hitting object has a player tag
+		{
+			ResourceController.Food += AmountOfood; // Add wood to resourcecontroller
+			Destroy(gameObject);  // Destroy itself after it has been picked up
+		}
+	}
+}
